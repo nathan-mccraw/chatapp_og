b581c59a722675333c9fcf0c35628eebe936d52d
nathan-mccraw <naterltw175@gmail.com>
2021-08-09 20:11:21 -0400

    Add sign-in modal. Install react-bootstrap.

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -6,63 +6,73 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const App = () => {
-    const [messageArray, setMessageArray] = useState("");
-    const [otherUsersArray, setOtherUsersArray] = useState("");
-    const [chatMessage, setChatMessage] = useState("");
-    const [user, setUser] = useState({
-        userId: "2",
-        userName: "Nate McCraw",
-        password: "nate",
-        firstName: "Nathan",
-        lastName: "McCraw",
-        birthday: "01/14/1987",
-        DateCreated: "08/07/2021",
+  const [messageArray, setMessageArray] = useState("");
+  const [otherUsersArray, setOtherUsersArray] = useState("");
+  const [chatMessage, setChatMessage] = useState("");
+  const [IsSignInModalOpen, setIsSignInModalOpen] = useState(true);
+  const [user, setUser] = useState({
+    userId: "1",
+    userName: "Guest",
+    password: "",
+    firstName: "Guest",
+    lastName: "Guest",
+    DateCreated: "08/07/2021",
+  });
+
+  useEffect(() => {
+    axios.get("/api/users").then((response) => {
+      setOtherUsersArray(response.data);
     });
 
-    useEffect(() => {
-        axios.get("/api/users").then((response) => {
-            setOtherUsersArray(response.data);
-        });
-
-        axios.get("/api/messages").then((response) => {
-            setMessageArray(response.data);
-        });
+    axios.get("/api/messages").then((response) => {
+      setMessageArray(response.data);
     });
+  });
 
-    const submitMessage = () => {
-        const message = {
-            messageId: Math.floor(Math.random() * 1000),
-            userId: user.userId,
-            userName: user.userName,
-            content: chatMessage,
-            dateCreated: "08/08/2021",
-            timeCreated: "1:01 AM",
-        };
-        setMessageArray(() => [...messageArray, message]);
-        console.log(messageArray);
+  const showSignInModal = () => setIsSignInModalOpen(true);
+  const hideSignInModal = () => setIsSignInModalOpen(false);
+
+  const submitMessage = (e) => {
+    e.preventDefault();
+    const message = {
+      messageId: Math.floor(Math.random() * 1000),
+      userId: user.userId,
+      userName: user.userName,
+      content: chatMessage,
+      dateCreated: "08/08/2021",
+      timeCreated: "1:01 AM",
     };
+    setMessageArray(() => [...messageArray, message]);
+    setChatMessage("");
+  };
 
-    return (
-        <div className="App container-fluid vh-100">
-            <div className="row vh-100">
-                <div className="col-md-auto p-0">
-                    <FriendsSideBar otherUsersArray={otherUsersArray} />
-                </div>
-                <div className="col vh-100 p-0">
-                    <div className="row overflow-auto m-0 h-75">
-                        <ChatWindow messageArray={messageArray} />
-                    </div>
-                    <div className="row m-0 h-25">
-                        <ChatInput
-                            submitMessage={submitMessage}
-                            chatMessage={chatMessage}
-                            setChatMessage={setChatMessage}
-                        />
-                    </div>
-                </div>
-            </div>
+  const signIn = () => {};
+
+  return (
+    <div className="App container-fluid vh-100">
+      <div className="row vh-100">
+        <div className="col-md-auto p-0">
+          <FriendsSideBar otherUsersArray={otherUsersArray} />
         </div>
-    );
+        <div className="col vh-100 p-0">
+          <div className="row overflow-auto m-0 h-75">
+            <ChatWindow messageArray={messageArray} />
+          </div>
+          <div className="row m-0 h-25">
+            <ChatInput
+              submitMessage={submitMessage}
+              chatMessage={chatMessage}
+              setChatMessage={setChatMessage}
+              user={user}
+              showSignInModal={showSignInModal}
+              hideSignInModal={hideSignInModal}
+              IsSignInModalOpen={IsSignInModalOpen}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- ClientApp/src/ChatInput.js
+++ ClientApp/src/ChatInput.js
@@ -1,27 +1,64 @@
 import React from "react";
+import Modal from "react-bootstrap/Modal";
+import SignInModal from "./SignInModal";
 
-const ChatInput = ({ submitMessage, ChatMessage, setChatMessage }) => {
+const ChatInput = ({
+  submitMessage,
+  ChatMessage,
+  setChatMessage,
+  user,
+  showSignInModal,
+  hideSignInModal,
+  IsSignInModalOpen,
+  signIn,
+}) => {
   return (
     <div className="col">
-      <div className="row m-3 mt-4 justify-content-center align-items-center m-0">
-        <div className="col-9">
-          <textarea
-            className="form-control"
-            rows="3"
-            placeholder="Chat..."
-            value={ChatMessage}
-            onChange={(e) => setChatMessage(e.target.value)}
-          ></textarea>
+      <form action="submit" onSubmit={submitMessage}>
+        <div className="row m-3 mt-4 mb-4 justify-content-center align-items-center">
+          <div className="col-9">
+            <textarea
+              className="form-control"
+              rows="3"
+              placeholder="Chat..."
+              value={ChatMessage}
+              onChange={(e) => setChatMessage(e.target.value)}
+              required
+            ></textarea>
+          </div>
+          <div className="col-auto">
+            <button type="submit" className="btn btn-outline-secondary">
+              Send
+            </button>
+          </div>
         </div>
-        <div className="col">
+      </form>
+      <div className="row mt4 justify-content-center align-items-baseline">
+        <div className="col-auto p-1">sending message as:</div>
+        <div className="col-auto p-1 h5 text-primary">
+          {user && user.userName}
+        </div>
+        <div className="col-auto p-1">
           <button
             type="button"
-            className="btn btn-outline-secondary"
-            onClick={submitMessage}
+            class="btn btn-sm btn-outline-primary  rounded-pill m-2"
+            onClick={showSignInModal}
           >
-            Send
+            Sign-in
           </button>
         </div>
+        <Modal
+          show={IsSignInModalOpen}
+          onHide={hideSignInModal}
+          dialogClassName={"SignInModal"}
+        >
+          <Modal.Header className="justify-content-center">
+            <h3>Sign In</h3>
+          </Modal.Header>
+          <Modal.Body>
+            <SignInModal signIn={signIn} />
+          </Modal.Body>
+        </Modal>
       </div>
     </div>
   );
