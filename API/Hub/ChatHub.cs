490f79a6fdfb509757b96bb335ea97d77dcb05aa
nathan-mccraw <naterltw175@gmail.com>
2021-09-27 14:48:32 -0400

    File restructor, add API folder. Add Hub folder with Chathub.

---
+++ API/Hub/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using ChatApp.Models;
+
+namespace ChatApp
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message)
+        {
+            await Clients.All.SendAsync("RecieveMessage", message);
+        }
+    }
+}
